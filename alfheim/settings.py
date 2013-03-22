89608b073c95389922e4e1ffc4b0418b16cba93b
appert44 <trichard@appert44.org>
2013-03-22 14:32:55 +0100

    refresh

--- alfheim/settings.py
+++ alfheim/settings.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'alfheimdb',
-        'USER': 'alfheim',
-        'PASSWORD': 'django',
-        'HOST': 'localhost',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '',
     }
 }
@@ -154,5 +154,5 @@ LOGGING = {
         },
     }
 }
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
