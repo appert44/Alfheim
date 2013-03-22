b765e6709530b054eefe186d68032c8990739d59
appert44.org <kmoreau@appert44.org>
2013-03-22 12:18:03 +0100

    commit

--- alfheim/settings.py
+++ alfheim/settings.py
@@ -114,7 +114,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'raven.contrib.django.raven_compat',
     'alfheimweb',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
