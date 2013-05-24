9d3c2884a450c1bf627902d8a615f64d5c0985e8
appert44.org <kmoreau@appert44.org>
2013-05-24 15:13:48 +0200

    maj

--- alfheimweb/cron.py
+++ alfheimweb/cron.py
@@ -5,8 +5,10 @@ from alfheimweb.models import *
 import datetime
 
 def Agregation():
+##### agrégation Hour
     H_startTime = None
     H_endTime = None
+    
     if H_agregation.objects.count() == 0:
         for lastline in TableBrut.objects.all().order_by('time'):
             lasttps = lastline.tps()
@@ -24,4 +26,24 @@ def Agregation():
     h_nbrow = TableBrut.objects.count()
     h_final = h_agrega / h_nbrow
     H_agregation.objects.create(time=H_startTime, value=h_final,device_id="001",sensor_type="temp")
+##### agrégation Day
+    D_startTime = None
+    D_endTime = None
+    
+    if D_agregation.objects.count() == 0:
+        for D_lastline in H_agregation.objects.all().order_by('time'):
+            D_lasttps = D_lastline.tps()
+        D_startTime= datetime.datetime(D_lasttps.year, D_lasttps.month, D_lasttps.day, D_lasttps.hour, 00, 00)
+        D_endTime= datetime.datetime(D_lasttps.year, D_lasttps.month, D_lasttps.day, D_lasttps.hour, 59, 59)
+    else:
+        for D_lastline in D_agregation.objects.all().order_by('time'):
+            D_lasttps = D_lastline.tps()
+        D_startTime= datetime.datetime(D_lasttps.year, D_lasttps.month, D_lasttps.day, D_lasttps.hour, 00, 00)
+        D_endTime= datetime.datetime(D_lasttps.year, D_lasttps.month, D_lasttps.day, D_lasttps.hour, 59, 59)
+    h_agrega = 0
 
+    for D_create in H_agregation.objects.filter(Q(time__gte=D_startTime) | Q(time__lte=D_endTime)):
+        D_agrega = D_agrega + D_create.val()
+    D_nbrow = H_agregation.objects.count()
+    D_final = D_agrega / D_nbrow
+    D_agregation.objects.create(time=D_startTime, value=D_final,device_id="001",sensor_type="temp")
